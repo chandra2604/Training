a208eaac169696ce0a7bfc3eeb95717fe83eb60a
chandra2604 <117428337+chandra2604@users.noreply.github.com>
2022-11-21 17:42:32 +0530

    first custom components

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import Todo from './Todo';
+import ExpenseItem from './ExpenseItem';
+import './App.css';
 
-function App(){
-    return(
-        <div>
-            <h1>My Todo</h1>
-            <Todo text='Learn react'/>
-        </div>
-    );
-        
+function App() {
+  const expense=[
+    {
+      id:"e1",
+      title: "Paper",
+      amount:94.2,
+      date:new Date(2020,3,23),
+    },
+    {id:"e2",title:"Car Insurance", amount:99.65,date:new Date(2020,4,24)},
+    {id:"e3", title:"New Desk",amount:99.74,date:new Date(2020,5,25)},
+  ];
+
+  
+  return (
+    <div>
+      <h1>Let's Get Started!</h1>
+      <ExpenseItem
+       title={expense[0].title}
+      amount={expense[0].amount}
+      date={expense[0].date}
+      >
+      </ExpenseItem>
+      <ExpenseItem title={expense[1].title}
+      amount={expense[1].amount}
+      date={expense[1].date}
+      >
+      </ExpenseItem>
+      <ExpenseItem title={expense[2].title}
+      amount={expense[2].amount}
+      date={expense[2].date}
+      >
+      </ExpenseItem>
+    </div>
+  );
 }
+
 export default App;
